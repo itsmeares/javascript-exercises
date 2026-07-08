a7e7dfc10aa454bfc7dfa143563116fe3bc3c7f6
Aras <72440556+itsmeares@users.noreply.github.com>
2026-07-08 06:13:32 +0100

    Correction for 04_mathEquations

--- foundations/04_mathEquations/mathEquations.js
+++ foundations/04_mathEquations/mathEquations.js
@@ -3,7 +3,7 @@ const b = 1 + 8
 const c = 22 * 3
 const d = 5 % 4
 const e = b - 17
-const f = 0 + 9 + 66 + 1 + -8
+const f = a + b + c + d + e // Correction here: I made this like "0 + 9 + 66 + 1 + -8" before but I should've used variables instead, both give the same result however sum of variables are makes more sense instead of sum of results because if a variable changes the f variable wouldn't work correctly
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
