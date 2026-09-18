9e2be4073c5f27147f56ef2b9d9ed03cf18192c1
Aras <72440556+itsmeares@users.noreply.github.com>
2026-09-18 15:44:03 +0100

    Add solution

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const originalString = string;
+    if (num > 0) {
+        for (let i = 1; i < num; i++) {
+            string += originalString;
+        } return string;
+    } else if (num < 0) {
+        return "ERROR";
+    } else return "";
 };
 
 // Do not edit below this line
