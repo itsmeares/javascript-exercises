3dc8dd1078ce35cfcc1d5d805da3cff884dc8bfd
Aras <72440556+itsmeares@users.noreply.github.com>
2026-09-18 21:30:24 +0100

    Add solution

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== "number" || typeof end !== "number") {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
 
+    let result = 0
+
+    if (start <= end) {
+        for (let i = start; i <= end; i++) {
+            result += i;
+        } return result;
+    } else if (start >= end) {
+        for (let i = end; i <= start; i++) {
+            result += i;
+        } return result;
+    }    
 };
 
 // Do not edit below this line
